7c1ed0d57e9c765de72d40f3be9269d87fdcb7a1
yuger <1527610316@qq.com>
2016-12-27 23:49:10 +0800

    Initial commit

---
+++ Webserver/src/main/java/com/wxy/androidwebserver/AndroidWebServer.java
@@ -0,0 +1,29 @@
+package com.wxy.androidwebserver;
+
+import java.util.Map;
+
+import fi.iki.elonen.NanoHTTPD;
+
+
+public class AndroidWebServer extends NanoHTTPD {
+
+    public AndroidWebServer(int port) {
+        super(port);
+    }
+
+    public AndroidWebServer(String hostname, int port) {
+        super(hostname, port);
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String msg = "<html><body><h1>Hello server</h1>\n";
+        Map<String, String> parms = session.getParms();
+        if (parms.get("username") == null) {
+            msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
+        } else {
+            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+        }
+        return newFixedLengthResponse(msg + "</body></html>\n");
+    }
+}
